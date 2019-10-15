84c1053c16cad36c3425d6b161aebe5b955dfd98
kompotkot <7047457+kompotkot@users.noreply.github.com>
2019-10-15 14:36:12 +0300

    Translated some comments

--- RoomMassingScript.py
+++ RoomMassingScript.py
@@ -16,19 +16,19 @@ from Autodesk.Revit.DB.Architecture import RoomFilter
 from RevitServices.Persistence import DocumentManager
 from System import Guid
 
-#PREDEFINED VARIABLES
+# PREDEFINED VARIABLES
 app = __revit__.Application
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
 selection = __revit__.ActiveUIDocument.Selection
 
 
-#PARAMS
-forFilterFlatType = 'ROM_Type of Layout'		#Для получения нужной строки из комнаты для названия материала
-forFilterLevel = "02" 							#Для фильтра по уровню
-forFilterTower = "R1"							#Для фильтра по башне
+# PARAMS
+forFilterFlatType = 'ROM_Type of Layout'				# Getting row from room properties for material name
+forFilterLevel = "02" 							# For level filter 
+forFilterTower = "R1"							# For tower filter
 
-def materialname(materialname,docname=doc): 	#Берет материал из mname. На ввод Имя материала и Документ с которым работаем. Можно обрать от имени материала Id
+def materialname(materialname,docname=doc): 	# Takes material from mname. Input: Material name and working Document. Possible to draw on behalf of the material Id
 	collectorMaterials = FilteredElementCollector(docname)
 	matitr = collectorMaterials.WherePasses(ElementClassFilter(Material)).ToElements()
 	matname = None
@@ -38,7 +38,7 @@ def materialname(materialname,docname=doc): 	#Берет материал из m
 			break
 	return matname
 
-def materialcreator(newmaterialname, docname=doc):		#Создает материал
+def materialcreator(newmaterialname, docname=doc):			# Create material
 	collectorMaterials = FilteredElementCollector(docname)
 	matitr = collectorMaterials.WherePasses(ElementClassFilter(Material)).ToElements()
 	listofmaterials = []
@@ -49,24 +49,24 @@ def materialcreator(newmaterialname, docname=doc):		#Создает матери
 	else:
 		Material.Create(docname, newmaterialname)
 
-#Use a RoomFilter to find all room elements in the document
+# Use a RoomFilter to find all room elements in the document
 roomfilter = RoomFilter()
-collectorRooms = FilteredElementCollector(doc)		#Apply the filter to the elements in the active document
+collectorRooms = FilteredElementCollector(doc)		# Apply the filter to the elements in the active document
 collectorRooms.WherePasses(roomfilter)
-roomiditr = collectorRooms.GetElementIdIterator()	#Get result as ElementId iterator
+roomiditr = collectorRooms.GetElementIdIterator()	# Get result as ElementId iterator
 roomiditr.Reset()
-opt = SpatialElementBoundaryOptions()				#Boundary options for rooms
+opt = SpatialElementBoundaryOptions()			# Boundary options for rooms
 
-#Пробегаемся по каждой существующей комнате
+# Cycle through each existing room
 while (roomiditr.MoveNext()):
-	roomid = roomiditr.Current						#Вытащить roomid
-	room = doc.GetElement(roomid) 					#Вытащить комнату по roomid
+	roomid = roomiditr.Current						# Take roomid
+	room = doc.GetElement(roomid) 						# Take room by roomid
 	roomLevel = room.get_Parameter(BuiltInParameter.ROOM_LEVEL_ID).AsValueString()
 	roomTower = room.LookupParameter('ROM_Number (Section)').AsString()
-	roomFlatType = room.LookupParameter(forFilterFlatType).AsString()		#Получаем название тима квартиры для названия материала
+	roomFlatType = room.LookupParameter(forFilterFlatType).AsString()		# Get name of room type for material name
 	if roomLevel == forFilterLevel and roomTower == forFilterTower and roomFlatType != None:
 		
-		#Отбираем высоту помещения
+		# Get room height
 		if type(room.get_Parameter(BuiltInParameter.ROOM_HEIGHT).AsValueString()) == str:
 			
 			roomHeightMm = room.get_Parameter(BuiltInParameter.ROOM_HEIGHT).AsValueString()
@@ -77,89 +77,89 @@ while (roomiditr.MoveNext()):
 		else:
 			roomHeightFt = int(10)
 
-		#Загружаем шаблон семейства
+		# Load family temlate
 		docFam = app.NewFamilyDocument("C:\\ProgramData\Autodesk\\RVT 2016\\Family Templates\\English\\Metric Generic Model.rft")
 		docFam.SaveAs("D:\\Temp\\" + room.UniqueId.ToString() + ".rfa")
 
-		#Начинаем транзакцию для модели
+		# Start transaction for model
 		m_Trans = Transaction(doc, 'Model transaction to create room boundaries')
 		m_Trans.Start()
-		#Начинаем транзакцию для семейства
+		# Start transaction for family
 		m_TransFam = Transaction(docFam, 'Family transaction to create each Family')
 		m_TransFam.Start()
 
-		#Работаем с контурами помещения
-		rvBoundary = room.GetBoundarySegments(opt) #Get room boundary
-		#Фильтруем помещение если неудалось выдавить форму
+		# Working with room contours
+		rvBoundary = room.GetBoundarySegments(opt) 	# Get room boundary
+		# Filter room if there are no possibility to generate from
 
-		for rvLoop in rvBoundary: 			#For each loop in the room boundary
-			crvarr = CurveArray()			#Здесь будет кривая для каждой комнаты объединенная из отрезков
-			for rvPiece in rvLoop:			#Retrieve each segment of the loop
-				dsPiece = rvPiece.Curve 	#Трансформируем в отрезки
-				crvarr.Append(dsPiece)		#Добавляем отрезки в Кривую crvarr
+		for rvLoop in rvBoundary: 			# For each loop in the room boundary
+			crvarr = CurveArray()			# There will be curve fro each room segments 
+			for rvPiece in rvLoop:			# Retrieve each segment of the loop
+				dsPiece = rvPiece.Curve 	# Transform to segments
+				crvarr.Append(dsPiece)		# Add segments to curve crvarr
 
-			#Генерация форм в семействе
+			# Form generation in family
 			ptOrigin = XYZ(0,0,0)
 			ptNormal = XYZ(0,0,1)
 			plane = app.Create.NewPlane(ptNormal, ptOrigin)
 			sketchPlane = SketchPlane.Create(docFam, plane)
-			#Преобразуем контур. Хз чем отличется CurveArrArray от CurveArray
+			# Convert the outline. No idea waht difference CurveArrArray of CurveArray 
 			curveArrArray = CurveArrArray();
 			curveArrArray.Append(crvarr);
-			#Выдавливаем форму. Фильтруем если не удалось выдавить
+			# Generate from. Filter out if no success
 			try:
 				extrusion = docFam.FamilyCreate.NewExtrusion(True, curveArrArray, sketchPlane, roomHeightFt)
-				print('Форма выдавилась')
+				print('Form generated')
 			except:
-				print('Неудалось выдавить форму')
+				print('Form can\'t be generated')
 				break
 
-			#Создаем материал в семействе
+			# Create material in family
 			matforflattype = "PNT-" + roomFlatType + "-FormAlgorithm"
 			materialcreator(matforflattype,docFam)
 
-			#Find the first geometry face of the given extrusion object
+			# Find the first geometry face of the given extrusion object
 			geomElement = extrusion.get_Geometry(Options())
 			geoObjectItor = geomElement.GetEnumerator()
 			while (geoObjectItor.MoveNext()):
-				solid = geoObjectItor.Current		# need to find a solid first
-				for face in solid.Faces:			# берем каждую поверхность и красим ее
+				solid = geoObjectItor.Current			# Need to find a solid first
+				for face in solid.Faces:			# Take each surface and paint it
 					docFam.Paint(extrusion.Id, face, materialname(matforflattype,docFam).Id)
 
 
-		#Завершаем транзакцию для семейства
+		# Finish transaction for family
 		m_TransFam.Commit()
 		docFam.Save()
-		#Завершаем транзакцию для модели
+		# Finish transaction for model
 		m_Trans.Commit()
 
-		#Загружаем семейство в модель
+		# Load family in model
 		family = docFam.LoadFamily(doc)
 
-		#ElementId iterator for Family
+		# ElementId iterator for Family
 		collectorFamilySymbols = FilteredElementCollector(doc)
 		collectorFamilySymbols.OfClass(FamilySymbol)
 		famtypeitr = collectorFamilySymbols.GetElementIdIterator()
 		famtypeitr.Reset()
 
-		#Пробегаемся по каждому существующему семейству в модели и сравниваем
-		familySymbol = None 	#Глобальная переменная в которую пишем из локальной в цикле
+		# Go through each existing family in model and compare
+		familySymbol = None 	# Global var, write in from local in cycle
 
 		"""
-		#Комментарии от Алексея
+		# Comments
 		cl_sheets = FilteredElementCollector(doc)
 		allsheets = cl_sheets.OfCategory(BuiltInCategory.OST_Sheets)
 		allviews = allviews.UnionWith(allsheets).WhereElementIsNotElementType().ToElements()
-		a = filter(lambda x: x.Name == family.Name.ToString(),famtypeitr) 	#Не позволяет взять Name от х, как и FamilyName
+		a = filter(lambda x: x.Name == family.Name.ToString(),famtypeitr) 	# No possibility to take Name from x, like FamilyName
 		"""
 
 		#doc.GetElement(famtypeitr.Current).FamilyName
 
 		while (famtypeitr.MoveNext()):
-			famid = famtypeitr.Current	#Вытащить familyid
-			fam = doc.GetElement(famid)	#Вытащить семейство по famid
+			famid = famtypeitr.Current	# Get familyid
+			fam = doc.GetElement(famid)	# Get family by famid
 			famname = fam.FamilyName
-			if family.Name.ToString() == famname: 	#Сравниваем название созданного семейства и название семейства в модели
+			if family.Name.ToString() == famname: 	# Compare name of created family and name of family in model
 				#print(family.Name.ToString() + " eaqual " + famname)
 				familySymbol = fam
 				break
@@ -170,11 +170,11 @@ while (roomiditr.MoveNext()):
 		m_Trans = Transaction(doc, 'Model transaction to place the Family into the model')
 		m_Trans.Start()
 
-		#Активируем Symbol
+		# Activate Symbol
 		familySymbol.Activate()
-		#Размещаем семейство
+		# Place family
 		familyInstance = doc.Create.NewFamilyInstance(XYZ(0, 0, 0), familySymbol, Structure.StructuralType.NonStructural)
 		
 		m_Trans.Commit()
 
-TaskDialog.Show("Done", "Did it.")
+TaskDialog.Show("Massing Done.", "Did it.")
